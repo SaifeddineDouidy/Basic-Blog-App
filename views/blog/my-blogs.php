c73b06f313babcfcfbb5c837ff0017cba1f10692
JunSaif <douidysifeddine@gmail.com>
2024-05-06 21:22:37 +0100

    First commit

--- views/blog/my-blogs.php
+++ views/blog/my-blogs.php
@@ -17,10 +17,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.php?route=my-blogs">Blogs </a>
+                        <a class="nav-link" href="index.php?route=all-blogs">Blogs </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php?route=all-blogs">My Blogs <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="index.php?route=my-blogs">My Blogs <span class="sr-only">(current)</span></a>
                     </li>
                 </ul>
             </div>
